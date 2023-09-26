f8fa84fa78e8e3ce107e69cfec1c5ff2288cb771
Marc Collado <marc@collado.io>
2023-09-26 12:18:02 +0200

    Update siteMetadata

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,33 +1,33 @@
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
       site {
         siteMetadata {
-          author {
-            name
-            summary
-          }
+          title
+          author
+          description
           social {
             twitter
           }
         }
       }
     }
-  `)
+  `);
 
-  const author = data.site.siteMetadata?.author
-  const social = data.site.siteMetadata?.social
+  const author = data.site.siteMetadata.author;
+  const description = data.site.siteMetadata.description;
+  const social = data.site.siteMetadata.social;
 
   return (
     <div className="bio">
       <StaticImage
         className="bio-avatar"
         layout="fixed"
-        formats={["auto", "webp", "avif"]}
+        formats={['auto', 'webp', 'avif']}
         src="../images/fat-art.jpg"
         width={70}
         height={70}
@@ -35,13 +35,13 @@ const Bio = () => {
         alt="Profile picture"
       />
       <p>
-        {author.summary} — amb <strong>{author.name}</strong>.<br></br>
+        {description} — amb <strong>{author.name}</strong>.<br></br>
         <a href={social?.twitter || ``}>
-          De fet, els podries de seguir a Twitter.
+          De fet, ens podries seguir a Twitter.
         </a>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Bio
+export default Bio;
